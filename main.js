43bb4d4f4276a3a2f6f7393072487d419f0b0740
Dilpreet Singh <singhsince94@gmail.com>
2020-10-12 22:40:15 +0530

    adds linux target to package.json

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function createWindow () {
   })
   win.setMenuBarVisibility(false)
   win.loadFile('index.html')
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
